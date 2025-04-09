16068bd3c22a8e0e7e9894ea3b880ac16e139c2d
Poojass01 <poojasuriakumar@gmail.com>
2025-04-09 15:53:46 +0530

    Add: compareTo method for line comparison

--- src/com/bridgelabz/linecomparison/Line.java
+++ src/com/bridgelabz/linecomparison/Line.java
@@ -1,7 +1,5 @@
-package com.bridgelabz.linecomparison;
-
-public class Line {
-    Point p1, p2;
+public class Line implements Comparable<Line> {
+    private Point p1, p2;
 
     public Line(Point p1, Point p2) {
         this.p1 = p1;
@@ -11,13 +9,17 @@ public class Line {
     public double length() {
         return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
     }
-}
 
-class Point {
-    int x, y;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Line)) return false;
+        Line other = (Line) obj;
+        return Double.compare(this.length(), other.length()) == 0;
+    }
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    @Override
+    public int compareTo(Line other) {
+        return Double.compare(this.length(), other.length());
     }
 }
